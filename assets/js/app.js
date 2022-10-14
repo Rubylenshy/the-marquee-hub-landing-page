6ad8cac83553495c3f3fb26ac05d6a6d4c1f8180
Rubylenshy <reztomoloju@gmail.com>
2022-10-14 12:07:39 +0100

    AOS added to images

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,24 @@ hamburger.addEventListener('click', ()=>{
     hamburger.classList.toggle('active')
     mobileNav.classList.toggle('active')
 })
+
+
+// Animate On Scroll (AOS)
+
+function scroll() {
+    var reveals = document.querySelectorAll(".scroll");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+    //   console.log(reveals.length);
+    //   console.log(windowHeight - 400)
+      if (elementTop < windowHeight - 300) {
+        reveals[i].classList.add("scrolled");
+      }else {
+        reveals[i].classList.remove("scrolled");
+      }
+    }
+    
+  }
+
+window.addEventListener("scroll", scroll);
